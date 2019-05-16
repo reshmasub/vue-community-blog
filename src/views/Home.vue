6c78f35663c3f4da1710f7d1662c2ce8af8a4969
Reshma Subramaniam <hi2resh26@gmail.com>
2019-05-16 16:57:51 -0700

    Implemented home page pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,62 @@
 <template>
-<div class="row">
-    <div class="col-md-8 offset-md-2" v-if = "articles.data" v-for="article in articles.data" :key="article.id">
-        <Article :article="article" />
+  <div>
+    <div class="d-flex mt-4 justify-content-between">
+      <button @click="getPrevArticles()" :disabled="articles.prev_page_url === null" class="btn btn-warning">Previous page</button>
+      <button @click="getNextArticles()" :disabled="articles.next_page_url === null" class="btn btn-warning">Next page</button>
     </div>
-</div>
-
+     <div class="row" v-if="!loading">
+          <div
+        class="col-md-8 offset-md-2"
+      
+        v-for="article in articles.data"
+        :key="article.id"
+      >
+        <Article :article="article"/>
+      </div>
+      </div>
+      <div class="loader text-center" v-else>
+           <i class="fas fa-3x fa-spin fa-spinner"></i>
+      </div>
+  </div>
 </template>
 
 <script>
-import Axios from 'axios';
-import config from '@/config';
-import Article from '@/components/Article.vue'
+import Axios from "axios";
+import config from "@/config";
+import Article from "@/components/Article.vue";
 export default {
-    components : {
-Article
-    },
-mounted() {
+  components: {
+    Article
+  },
+  mounted() {
     this.getArticles();
-},
-data() {
-return {
-    articles : {}
-}
-},
-methods : {
-    getArticles(){
-Axios.get(`${config.apiUrl}/articles`).then(response => {
-    this.articles = response.data.data;
-})
+  },
+  data() {
+    return {
+      articles: {},
+      loading: true
+    };
+  },
+  methods: {
+    getArticles(url = `${config.apiUrl}/articles`) {
+        this.loading = true;
+      Axios.get(url).then(response => {
+          this.loading = false;
+        this.articles = response.data.data;
+      });
+    },
+    getNextArticles(){
+        this.getArticles(this.articles.next_page_url);
+    },
+    getPrevArticles(){
+         this.getArticles(this.articles.prev_page_url);
     }
-}
-
-}
+  }
+};
 </script>
 
-<style>
-
+<style> 
+.btn-warning{
+    color: white;
+}
 </style>
