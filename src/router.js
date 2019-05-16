58174d384dabfcf573e81503582553ecf62d3db0
Reshma Subramaniam <hi2resh26@gmail.com>
2019-05-16 14:59:56 -0700

    Single article details implemented

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
 import Home from './views/Home.vue'
-
+import Article from './views/Article.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -17,6 +17,9 @@ const router = new Router({
     },{
         path : '/',
         component: Home
+     },{
+        path : '/article/:id',
+        component: Article
      }]
 })
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,85 +1,92 @@
 <template>
- <div class="row my-5">
-     <div class="col-md-6 offset-md-3">
-        <div class="card">
-            <div class="card-body">
-                <h3 class="text-center my-4">Login</h3>
-                 <div class="form-group">
-             <input v-bind:class="{'is-invalid' : errors.email}" v-model="email" type="text" placeholder="Email" class="form-control">
-             <div class="errors" v-if="errors.email">
-                 <small class="text-danger" :key="error" v-for="error in errors.email">{{error}}</small>
-             </div>
-         </div>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Login</h3>
           <div class="form-group">
-             <input v-bind:class="{'is-invalid' : errors.password}" v-model="password" type="password" placeholder="Password" class="form-control">
-              <div class="errors" v-if="errors.password">
-                 <small class="text-danger" :key="error" v-for="error in errors.password">{{error}}</small>
-             </div>
-         </div>
+            <input
+              v-bind:class="{'is-invalid' : errors.email}"
+              v-model="email"
+              type="text"
+              placeholder="Email"
+              class="form-control"
+            >
+            <div class="errors" v-if="errors.email">
+              <small class="text-danger" :key="error" v-for="error in errors.email">{{error}}</small>
+            </div>
+          </div>
+          <div class="form-group">
+            <input
+              v-bind:class="{'is-invalid' : errors.password}"
+              v-model="password"
+              type="password"
+              placeholder="Password"
+              class="form-control"
+            >
+            <div class="errors" v-if="errors.password">
+              <small class="text-danger" :key="error" v-for="error in errors.password">{{error}}</small>
+            </div>
+          </div>
           <div class="form-group text-center">
             <button @click="loginUser()" :disabled="loading" class="btn btn-success form-control">
-                <i class="fas fa-spin fa-spinner" v-if="loading"></i>
-                {{loading ? '' : 'Login'}}
+              <i class="fas fa-spin fa-spinner" v-if="loading"></i>
+              {{loading ? '' : 'Login'}}
             </button>
-         </div>
-            </div>
+          </div>
         </div>
-     </div>
- </div>
-  
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Axios from 'axios'
-import config from '@/config'
+import Axios from "axios";
+import config from "@/config";
 export default {
-    beforeRouteEnter(to, from, next){
-        
-        if(localStorage.getItem('auth')){
-            return next({path : "/"});
-        }
-        next();
-        
-    },
-data(){
+  beforeRouteEnter(to, from, next) {
+    if (localStorage.getItem("auth")) {
+      return next({ path: "/" });
+    }
+    next();
+  },
+  data() {
     return {
-        email : "",
-        password : "",
-        errors : {},
-        loading : false
-    }
-},
-methods : {
+      email: "",
+      password: "",
+      errors: {},
+      loading: false
+    };
+  },
+  methods: {
     loginUser() {
-        this.loading = true;
-        Axios.post(`${config.apiUrl}/auth/login`, {
-            email : this.email,
-            password : this.password
-        }).then((response) => {
-            this.loading = false;
-            this.$root.auth = response.data.data;
-            localStorage.setItem('auth',JSON.stringify(response.data.data));
-             this.$noty.success("Successfully logged in.")
-            this.$router.push("/")
-        }).catch(({response}) => {
-            this.loading = false;
-            this.$noty.error("Oops ! Something went wrong. ")
-            if(response.status === 401){
-                this.errors = {
-                    email : ["Credentials provided does not match our records"]
-                };
-            }
-            else {
-                this.errors = response.data;
-            }
-            
-            })
-        
+      this.loading = true;
+      Axios.post(`${config.apiUrl}/auth/login`, {
+        email: this.email,
+        password: this.password
+      })
+        .then(response => {
+          this.loading = false;
+          this.$root.auth = response.data.data;
+          localStorage.setItem("auth", JSON.stringify(response.data.data));
+          this.$noty.success("Successfully logged in.");
+          this.$router.push("/");
+        })
+        .catch(({ response }) => {
+          this.loading = false;
+          this.$noty.error("Oops ! Something went wrong. ");
+          if (response.status === 401) {
+            this.errors = {
+              email: ["Credentials provided does not match our records"]
+            };
+          } else {
+            this.errors = response.data;
+          }
+        });
     }
-}
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
